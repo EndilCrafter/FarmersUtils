6126603921bf07844b13a7d9e8a4d54693e1160e
shiro <shirokieiyuu@gmail.com>
2023-12-24 19:34:48 +0900

    fix recipe

--- src/main/java/net/endil/farmersutils/data/FUItemTags.java
+++ src/main/java/net/endil/farmersutils/data/FUItemTags.java
@@ -1,6 +1,8 @@
 package net.endil.farmersutils.data;
 
 import net.endil.farmersutils.FarmersUtils;
+import net.endil.farmersutils.common.registry.FUItems;
+import net.endil.farmersutils.common.tag.FUCompatTags;
 import net.endil.farmersutils.common.tag.FUTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
@@ -20,5 +22,13 @@ public class FUItemTags extends ItemTagsProvider {
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
         this.tag(FUTags.CRATE_CAN_CONTAIN).addTag(Tags.Items.CROPS);
+
+        this.tag(FUCompatTags.DIET_FRUITS)
+                .add(FUItems.APPLE_SLICE.get(), FUItems.GOLDEN_APPLE_SLICE.get(), FUItems.DRIED_APPLE_SLICE.get(), FUItems.DRIED_GOLDEN_APPLE_SLICE.get());
+        this.tag(FUCompatTags.DIET_PROTEINS)
+                .add(FUItems.DRIED_BEEF.get(), FUItems.DRIED_CHICKEN.get(), FUItems.DRIED_COD_SLICE.get(), FUItems.DRIED_MUTTON.get(),
+                        FUItems.DRIED_PORKCHOP.get(), FUItems.DRIED_RABBIT.get(), FUItems.DRIED_SALMON_SLICE.get(), FUItems.BOILED_EGG.get(),
+                        FUItems.DRIED_GOLDEN_BEEF.get(), FUItems.DRIED_GOLDEN_CHICKEN.get(), FUItems.DRIED_GOLDEN_COD_SLICE.get(), FUItems.DRIED_GOLDEN_MUTTON.get(),
+                        FUItems.DRIED_GOLDEN_PORKCHOP.get(), FUItems.DRIED_GOLDEN_RABBIT.get(), FUItems.DRIED_GOLDEN_SALMON_SLICE.get(), FUItems.BOILED_GOLDEN_EGG.get());
     }
 }
