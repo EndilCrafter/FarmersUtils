5d68f10d0cd81a70b697d29249d3ff84c0a83059
shiro <shirokieiyuu@gmail.com>
2023-12-26 07:03:34 +0900

    fix wrong code

--- src/main/java/net/endil/farmersutils/client/FUClientSetup.java
+++ src/main/java/net/endil/farmersutils/client/FUClientSetup.java
@@ -3,10 +3,10 @@ package net.endil.farmersutils.client;
 import net.endil.farmersutils.client.gui.screens.inventory.CrateScreen;
 import net.endil.farmersutils.common.registry.FUMenuTypes;
 import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class FUClientSetup {
-    public static void init(final FMLCommonSetupEvent event) {
+    public static void init(final FMLClientSetupEvent event) {
         MenuScreens.register(FUMenuTypes.CRATE.get(), CrateScreen::new);
     }
 }
